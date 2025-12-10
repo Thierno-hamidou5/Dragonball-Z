67c7280ff14e892319b299892124530a527e84a5
Thierno-hamidou5 <anderssonjayson@gmail.com>
2025-12-10 15:24:51 +0100

    upadted App.css, App.jsx and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,98 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import axios from "axios";
-import "./App.css";
-
-// Importiere Unterseiten-Komponenten
-import CharacterDetail from "./pages/CharacterDetail";
-import CharacterCard from "./components/CharacterCard.jsx";
-import Villains from "./pages/Villains";
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Z_Fighters from "./pages/Z_Fighters";
+import Villains from "./pages/Villains";
+import CharacterDetail from "./pages/CharacterDetail";
 import NeuCharacters from "./pages/NeuCharacters";
+import ManageCharacters from "./pages/ManageCharacters";
+import FavoritesPage from "./pages/FavoritesPage";
+import Login from "./pages/Login";
+import Forbidden from "./pages/Forbidden";
+import Home from "./pages/Home";
 
+/**
+ * SPA-Root: definiert alle Routen und schuetzt sie per ProtectedRoute,
+ * aehnlich wie die Klassenkommentare im Backend.
+ */
 function App() {
-  // Zustand für API-Charaktere
-  const [data, setData] = useState([]);
-  // Zustand für benutzerdefinierte Charaktere
   const [customCharacters, setCustomCharacters] = useState([]);
 
-  // Hole die API-Daten beim ersten Laden der Seite
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://dragonball-api.com/api/characters"
-        );
-
-        // Wenn Daten vorhanden sind, speichere sie im Zustand
-        if (response.data?.items) {
-          setData(response.data.items);
-        }
-      } catch (error) {
-        console.error("Fehler beim Laden der Charaktere:", error);
-      }
-    };
-
-    fetchData(); // Starte Datenabruf
-  }, []);
-
   return (
-    <div className="App">
-      {/* Navigation im oberen Bereich */}
-      <nav className="App-nav">
-        <Link to="/">Home</Link>
-        <Link to="/villains">Villains</Link>
-        <Link to="/z_fighters">Z Fighters</Link>
-        <Link to="/neucharacters">Neu Character</Link>
-      </nav>
-
-      {/* Routing der verschiedenen Seiten */}
       <Routes>
-        {/* Villains-Seite */}
-        <Route path="/villains" element={<Villains />} />
-
-        {/* Z Fighters-Seite */}
-        <Route path="/z_fighters" element={<Z_Fighters />} />
-
-        {/* Detailansicht eines Charakters */}
-        <Route
-          path="/character/:name"
-          element={
-            <CharacterDetail
-              customCharacters={customCharacters}
-              setCustomCharacters={setCustomCharacters}
-            />
-          }
-        />
-
-        {/* Seite zum Hinzufügen oder Bearbeiten von Charakteren */}
-        <Route
-          path="/neucharacters"
-          element={
-            <NeuCharacters
-              customCharacters={customCharacters}
-              setCustomCharacters={setCustomCharacters}
-            />
-          }
-        />
-
-        {/* Startseite – zeigt alle Charaktere an */}
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>Welcome to the Tournament</h1>
-
-              {/* Logo mit Link zur API-Webseite */}
-              <div className="logo-container">
-                <a
-                  href="https://web.dragonball-api.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/Logo.jpg"
-                    className="logo"
-                    alt="Dragon Ball Super"
+        <Route path="/" element={<Layout />}>
+          {/* Home zeigt alle Charaktere */}
+          <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+          />
+          {/* Z-Fighters separat unter /z-fighters */}
+          <Route
+              path="z-fighters"
+              element={
+                <ProtectedRoute>
+                  <Z_Fighters />
+                </ProtectedRoute>
+              }
+          />
+          {/* Villains separat unter /villains */}
+          <Route
+              path="villains"
+              element={
+                <ProtectedRoute>
+                  <Villains />
+                </ProtectedRoute>
+              }
+          />
+          <Route
+              path="characters/:id"
+              element={
+                <ProtectedRoute>
+                  <CharacterDetail
+                      customCharacters={customCharacters}
+                      setCustomCharacters={setCustomCharacters}
                   />
-                </a>
-              </div>
-
-              {/* Zeige alle API-Charaktere und eigene Charaktere */}
-              {[...data, ...customCharacters].map((character, i) => (
-                <CharacterCard
-                  key={character.id || character.name + i} // eindeutiger Key
-                  name={character.name}
-                  image={character.image}
-                  race={character.race}
-                  gender={character.gender}
-                  baseKi={character.ki}
-                  totalKi={character.maxKi}
-                  affiliation={character.affiliation}
-                  isCustom={i >= data.length} // Kennzeichne eigene Charaktere
-                />
-              ))}
-            </>
-          }
-        />
+                </ProtectedRoute>
+              }
+          />
+          <Route
+              path="new-characters"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <NeuCharacters
+                      customCharacters={customCharacters}
+                      setCustomCharacters={setCustomCharacters}
+                  />
+                </ProtectedRoute>
+              }
+          />
+          {/* Admin-only route for managing characters via the backend */}
+          <Route
+              path="manage-characters"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <ManageCharacters />
+                </ProtectedRoute>
+              }
+          />
+          <Route
+              path="favorites"
+              element={
+                <ProtectedRoute>
+                  <FavoritesPage />
+                </ProtectedRoute>
+              }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="forbidden" element={<Forbidden />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
       </Routes>
-    </div>
   );
 }
 
